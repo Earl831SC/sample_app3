f2216a9c83df8d3bd2a9d9d1f6ef7c5e12a309a1
Earl831SC <ryanjudd31@gmail.com>
2015-06-23 15:19:25 -0700

    add meta data for mobile

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp3::Application.routes.draw do
-  get "users/new"
+resources :users
 
   get "static_pages/home"
   get "static_pages/help"
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name "Ryan Judd"
+		email "ryan@gmail.com"
+		password "Pepper"
+		password_confirmation "Pepper"
+	end
+end
